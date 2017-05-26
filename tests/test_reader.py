653db6508752d3be73aae731c22020b25fab2602
Nate Vack <njvack@wisc.edu>
2017-05-26 15:47:00 -0500

    Make the reader test okay

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -5,28 +5,14 @@
 test_read_itek
 ----------------------------------
 
-Tests for `read_itek` module.
+Tests for `reader` module.
 """
 
 import pytest
 
 
-from read_itek import read_itek
+from read_itek import reader
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
+def test_content():
+    pass
